e07c17cc9a3599c47898b28bbbb88053ce1a04d8
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-10-05 08:41:35 +0200

    Replace class with factory function

--- src/Language.js
+++ src/Language.js
@@ -1,17 +1,15 @@
 'use strict'
 
-class Language {
-  constructor (iso6391Code, iso6392Code, iso6393Code, eng, nob, nno, sme) {
-    return {
-      uri: 'http://lexvo.org/id/iso639-3/' + iso6393Code,
-      iso6391Code: iso6391Code,
-      iso6392Codes: iso6392Code,
-      iso6393Code: iso6393Code,
-      eng: eng,
-      nob: nob,
-      nno: nno,
-      sme: sme
-    }
+const Language = (iso6391Code, iso6392Code, iso6393Code, eng, nob, nno, sme) => {
+  return {
+    uri: `http://lexvo.org/id/iso639-3/${iso6393Code}`,
+    iso6391Code: iso6391Code,
+    iso6392Codes: iso6392Code,
+    iso6393Code: iso6393Code,
+    eng: eng,
+    nob: nob,
+    nno: nno,
+    sme: sme
   }
 }
 
