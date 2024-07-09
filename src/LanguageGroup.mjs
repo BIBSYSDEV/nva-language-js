ba81de89599c75c43270e613f16de78eea745631
anetteOlli <anette.olli.siiri@sikt.no>
2024-07-09 14:20:15 +0200

    support-sami-language-group, codacy

--- src/LanguageGroup.mjs
+++ src/LanguageGroup.mjs
@@ -4,10 +4,10 @@ export default (iso6395Code, eng, nob, nno, sme) => {
     iso6391Code: null,
     iso6392Codes: [],
     iso6393Code: null,
-    iso6395Code: iso6395Code,
-    eng: eng,
-    nob: nob,
-    nno: nno,
-    sme: sme
+    iso6395Code,
+    eng,
+    nob,
+    nno,
+    sme
   }
 }
